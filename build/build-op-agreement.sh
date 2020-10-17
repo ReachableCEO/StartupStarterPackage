5cfc80f2f48d560e9884eaee6b3f09e7acf5e373
Charles Wyble <charles@turnsys.com>
2020-10-17 00:07:29 -0500

    i think this will work..

---
+++ build/build-op-agreement.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+OUTPUT_BASE_DIR="../YOURLLC"
+LLC_NAME="YourLLC"
+
+PDF_OUTPUT_FILE="$OUTPUT_BASE_DIR/$LLC_NAME-Contract.pdf"
+MD_OUTPUT_FILE="$OUTPUT_BASE_DIR/$LLC_NAME-Contract.md"
+
+source common.sh
+
+prevRunCleanup
+makeOutput
---
+++ build/common.sh
@@ -0,0 +1,47 @@
+function prevRunCleanup()
+{
+ 
+rm $MD_OUTPUT_FILE
+rm $PDF_OUTPUT_FILE
+
+}
+
+function makeOutput()
+
+{
+#Company specfic bits
+company_files=(
+"../SingeLLCOpAgreement/title.md"
+"../SingeLLCOpAgreement/CompanyNameAndPurpose.md"
+"../SingeLLCOpAgreement/grantToMembers.md"
+)
+
+#Common template stuff
+boilerplate_files=(
+"../CommonBoilerplate/defs.md"
+"../CommonBoilerplate/boilerplate.md"
+"../CommonBoilerplate/profInterest.md"
+"../CommonBoilerplate/remove.md"
+"../CommonBoilerplate/nda.md"
+"../CommonBoilerplate/ip-assign.md"
+"../CommonBoilerplate/TxLLC-ForProfit-MultiMember-Series-ProfitInterestGrant-OpAgreement.md"
+)
+
+IFS=$'\n\t'
+
+for file in ${company_files[@]}; do
+	cat $file >> $MD_OUTPUT_FILE
+done
+
+for file in ${boilerplate_files[@]}; do
+	cat $file >> $MD_OUTPUT_FILE
+done
+
+pandoc \
+	< $MD_OUTPUT_FILE \
+	--number-sections \
+	--toc \
+	--from=markdown \
+	--to=pdf \
+	--output=$PDF_OUTPUT_FILE
+}
